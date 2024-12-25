3c08d61c937ea2903d2d1733913603bd68092f98
yashsharma8433 <yashsharma843347@gmail.com>
2024-12-26 01:55:37 +0530

    Initial commit with backend setup server running

---
+++ backend/app.js
@@ -0,0 +1,12 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require ('express');
+const cors = require ('cors');
+const app = express();
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+}     );
+
+module.exports = app;
